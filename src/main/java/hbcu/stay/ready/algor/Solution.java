208b66a22adf46f665b33f234081c8989de2c319
Carl Levine Jr <clevine313@gmail.com>
2019-08-06 09:59:44 -0400

    i feel pretty close

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,31 @@
 package hbcu.stay.ready.algor;
 
+import java.util.HashMap;
+
 public class Solution {
 
     public Boolean harmlessRansomNote(String message , String magazineText){
+
+        String[] output = magazineText.split(" ");
+        HashMap<String,Integer> newmap = new HashMap<>();
+
+        for(String x: output){
+            if(newmap.containsKey(x)){
+                newmap.put(x,newmap.get(x) + 1);
+            }
+            else{
+                newmap.put(x,1);
+            }
+        }
+
+        String[] output2 = message.split(" ");
+        for(String x: output2){
+            if(newmap.containsKey(x)&& newmap.containsValue(1)){
+                System.out.println(x);
+            }
+        }
+
+
         return null;
     }
 }
